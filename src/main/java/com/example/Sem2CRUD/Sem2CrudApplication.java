043726b24f69e33b35f8cf66b5985666e32cb772
Elena <0koshko0@gmail.com>
2024-02-09 19:00:24 +0300

    first

---
+++ src/main/java/com/example/Sem2CRUD/Sem2CrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sem2CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem2CrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sem2CRUD/Sem2CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sem2CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
